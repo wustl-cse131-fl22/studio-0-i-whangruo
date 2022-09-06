f357582e4dcede62f6737e87832c859dde174ea4
ruohanwang <ruohanwang@wrhs-mbp>
2022-09-05 23:36:11 -0500

    Hi

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -9,7 +9,7 @@ public class BurritoBudget {
 		int burritosEatenWeek4 = 1;
 		
 		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
-		double averageWeeklyBurritos = totalBurritos / 4;
+		int averageWeeklyBurritos = totalBurritos / 4;
 		
 		double burritoPrice = 7.50;
 		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
